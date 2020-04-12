f502c0f06e7602083911b7656a37dfcb51893d8a
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-12 17:05:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.include? do | valid_calls |
-    
+
   end
   end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
