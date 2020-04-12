eeeb0c8b527f150bbee18fa0e371f1ec4830575f
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-12 16:44:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   planeteer_calls.any? do |string|
-
+ if string.length === 4
+  
    end
   # Return the boolean result of this check
 end
