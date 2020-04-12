ae678221d9085e4335f33be277156caf36b64bf2
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-12 17:13:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find? do | calls |
-  e nd
+  end
   valid_calls.include?(calls)
 end
 
