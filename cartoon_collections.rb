c3294788dce8edfd4a9e0388438081276a8d623f
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-12 16:18:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
 def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
+  array.map do |element|
+    element*element
 end
 
 def summon_captain_planet(planeteer_calls)
@@ -15,6 +17,6 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
+  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
 end
